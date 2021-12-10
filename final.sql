823cd86e028297ea31a3a3cf34fdd2d329877d6e
chloejepson <jepsoc@rpi.edu>
2021-12-10 01:53:36 -0500

    Opening the branch to make sure I can run it on my laptop with the password hashing for our presentation tmrw

--- final.sql
+++ final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 08, 2021 at 06:04 AM
+-- Generation Time: Dec 10, 2021 at 07:51 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 7.4.16
 
@@ -39,16 +39,11 @@ CREATE TABLE `claimed` (
 --
 
 INSERT INTO `claimed` (`item`, `name`, `rin`, `email`) VALUES
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('book', 'lubna', 90, 'lubna@rpi.edu'),
-('book', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('book', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
-('me', 'Chloe Jepson', 90, 'jepsoc@rpi.edu');
+('Ring', 'Chloe Jepson', 90, 'jepsoc@rpi.edu'),
+('Book', 'Lubna', 32, 'lubna@rpi.edu'),
+('Hat', 'Rex', 80, 'rex@rpi.edu'),
+('Black Tshirt', 'Michelle', 78, 'michelle@rpi.edu'),
+('Pencil', 'Tom', 34, 'tom@rpi.edu');
 
 -- --------------------------------------------------------
 
@@ -69,10 +64,10 @@ CREATE TABLE `discussion` (
 --
 
 INSERT INTO `discussion` (`name`, `email`, `discription`, `rin`, `date`) VALUES
-('Chloe Jepson', 'jepsoc@rpi.edu', 'hi!', 90, '1/11/12'),
 ('Lubna', 'lubna@rpi.edu', 'we fixed it ', 90, '34/34/34'),
-('Michelle Lynn Jepson', 'mjepson@rpi.edu', 'lost book', 90, '2/3/4'),
-('Erin', 'erin@rpi.edu', 'working on midterms!', 90, '1/1/1');
+('Erin', 'erin@rpi.edu', 'working on midterms!', 90, '1/1/1'),
+('Chloe Jepson', 'jepsoc@rpi.edu', 'Has anyone seen a blue book in the union?', 661993521, '12/10/21'),
+('Rex', 'rex@rpi.edu', 'I found a key on the ground near the Rahps Dorms', 78, '12/11/21');
 
 -- --------------------------------------------------------
 
@@ -96,12 +91,9 @@ CREATE TABLE `found` (
 --
 
 INSERT INTO `found` (`item`, `name`, `image`, `description`, `rin`, `email`, `location`, `LorF`) VALUES
-('me', 'Chloe Jepson', 'profilepic.jpg', 'picture of me', 90, 'jepsoc@rpi.edu', 'union', 'found'),
-('me', 'Chloe Jepson', 'profilepic.jpg', 'picture of me', 90, 'jepsoc@rpi.edu', 'union', 'found'),
-('me', 'Chloe Jepson', 'profilepic.jpg', 'picture of me', 90, 'jepsoc@rpi.edu', 'union', 'found'),
-('me', 'Chloe Jepson', 'profilepic.jpg', 'picture of me', 90, 'jepsoc@rpi.edu', 'union', 'found'),
-('book', 'Chloe Jepson', 'pie-chart.png', 'bio textbook', 90, 'jepsoc@rpi.edu', 'folsom', 'found'),
-('book', 'xhloe', 'pie-chart.png', 'pie chart image', 32, 'jepsoc@rpi.edu', 'union', 'lost');
+('Textbook', 'Chloe Jepson', 'imageOfBook.png', 'Blue bio textbook', 661993521, 'jepsoc@rpi.edu', 'Union', 'Found'),
+('Ring', 'Lubna Ismail', 'imageOfRing.jpg', 'Diamond ring', 661995683, 'lubna@rpi.edu', 'Folsom Library', 'Found'),
+('IPhone', 'Paul Jang', 'imageOfPhone.png', 'Black IPhone XR', 661993233, 'paul@rpi.edu', 'Lally', 'found');
 
 -- --------------------------------------------------------
 
@@ -114,16 +106,19 @@ CREATE TABLE `users` (
   `password` text NOT NULL,
   `name` varchar(100) NOT NULL,
   `id` varchar(100) NOT NULL,
-  `is_admin` tinyint(4) NOT NULL
+  `is_admin` tinyint(4) NOT NULL,
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `name`, `id`, `is_admin`) VALUES
-('user1', 'password1', 'bob', '90', 0),
-('user2', 'password2', 'bob2', '90', 0);
+INSERT INTO `users` (`username`, `password`, `name`, `id`, `is_admin`, `email`) VALUES
+('user1', 'password1', 'bob', '90', 0, ''),
+('user2', 'password2', 'bob2', '90', 0, ''),
+('chloej', '$2y$10$ZdXTMCPMrahtDd9nCfXJVOAB2TYLXT628cAsPz6vjAs4iOHzDXl3i', 'Chloe Jepson', '', 0, 'jepsoc@rpi.edu'),
+('user3', '$2y$10$RGfhC8fD31A1e.3AeArJju0KwkKXXnOgzas.67FxW9FGVylJblZWK', 'Michelle', '', 0, 'michelle@rpi.edu');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
